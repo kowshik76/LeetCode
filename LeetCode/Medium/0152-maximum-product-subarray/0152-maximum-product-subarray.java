1aea569bfb9b681c0cec09507623d327eea5687f
Sai Kowshik Sangada <kowshiksangada@gmail.com>
2025-07-31 11:30:42 +0530

    Time: 1 ms (90.5%), Space: 44.9 MB (72.9%) - LeetHub

---
+++ LeetCode/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int product = 1;
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            product *= nums[i];
+            maxi = Math.max(product, maxi);
+            if (product == 0)
+                product = 1;
+        }
+                product = 1;
+        for (int i = nums.length-1; i >=0 ; i--) {
+            product *= nums[i];
+            maxi = Math.max(product, maxi);
+            if (product == 0)
+                product = 1;
+        }
+        return maxi;
+    }
+}
