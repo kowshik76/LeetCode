8b9bb4ab6f6205001ee36b8f88ee0f7765708e88
Sai Kowshik Sangada <kowshiksangada@gmail.com>
2025-11-06 12:33:03 +0530

    Implement Kth smallest element in BST

---
+++ Binary  Search Trees/230-Kth-smallest-Element-in-a-Bst/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        int count = 0;
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                count++;
+                if (count == k) {
+                    return curr.val;
+                }
+                curr = curr.right;
+            } else {
+                TreeNode prev = curr.left;
+                while (prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+                if (prev.right == null) {
+                    prev.right = curr;
+                    curr = curr.left;
+                } else {
+                    prev.right = null;
+                    count++;
+                    if (count == k) {
+                        return curr.val;
+                    }
+                    curr = curr.right;
+                }
+            }
+        }
+        return -1;
+    }
+}
